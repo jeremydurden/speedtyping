611222dc74c85a62a6767959848a91f4dee74e8d
Jeremy Durden <jeremydurden@gmail.com>
2022-03-25 18:02:56 -0700

    setting up initial state and handleChange function on the text area to update the value

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
 
+import { useState } from "react";
+
 function App() {
+  const [data, setData] = useState({ textarea: "" });
+
+  console.log(data);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setData((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
   return (
     <div className="App">
-      <h1 className="Title">Title</h1>
-      <textarea name="textarea" id="textarea" cols="30" rows="10" />
+      <h1 className="Title">How fast can you type?</h1>
+      <textarea
+        name="textarea"
+        id="textarea"
+        cols="30"
+        rows="10"
+        value={data.textarea}
+        onChange={handleChange}
+      />
       <h4>Time Left: </h4>
       <button>Start</button>
       <h1>Word Count: </h1>
