aaded9d7a9e7ea7651358613a831c7ae4a6ecbe1
Jeremy Durden <jeremydurden@gmail.com>
2022-03-26 13:05:32 -0700

    adding a useEffect and logict for the countdown timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [data, setData] = useState({ textarea: "", wordCount: 0 });
+  const [data, setData] = useState({
+    textarea: "",
+    wordCount: 0,
+  });
 
-  console.log(data);
+  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    if (timeRemaining > 0 && start) {
+      setTimeout(() => {
+        setTimeRemaining((prevState) => prevState - 1);
+      }, 1000);
+    }
+  }, [timeRemaining, start]);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -14,9 +26,15 @@ function App() {
     });
   };
 
-  const wordCounter = () => {
-    const words = data.textarea.split(" ");
-    const count = words.length;
+  const startGame = () => {
+    if (!start) {
+      setStart(true);
+    }
+  };
+
+  const wordCounter = (text) => {
+    const words = text.trim().split(" ");
+    const count = words.filter((word) => word !== "").length;
     setData((prevState) => {
       return { ...prevState, wordCount: count };
     });
@@ -33,8 +51,9 @@ function App() {
         value={data.textarea}
         onChange={handleChange}
       />
-      <h4>Time Left: </h4>
-      <button onClick={wordCounter}>Start</button>
+      <h4>Time Left: {timeRemaining}</h4>
+      <button onClick={() => wordCounter(data.textarea)}>Count</button>
+      <button onClick={startGame}>Start</button>
       <h1>Word Count: {data.wordCount}</h1>
     </div>
   );
